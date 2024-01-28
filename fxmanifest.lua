ee4cc70e487e7e62de60d5815dab2127aa011167
UgDev <support@ugdev.cf>
2024-01-28 14:41:57 +0100

    Initial Release

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'ug-contextMenu'
+description 'Context Menu for UgCore by UgDev'
+author 'UgDev'
+version '3.5'
+url 'https://github.com/UgDevOfc/UgCore/'
+ui_page 'html/index.html'
+
+client_scripts {
+    'client/main.lua'
+}
+
+files {
+    'html/index.html',
+    'html/css/*.css',
+    'html/js/*.js'
+}
